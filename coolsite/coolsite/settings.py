fb2a91e0a5597add38047afceae0eb9092618865
Narxoz <manshuk.zholdasbayeva@narxoz.kz>
2023-03-12 17:45:19 +0600

    5-6 task2

--- coolsite/coolsite/settings.py
+++ coolsite/coolsite/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -119,12 +119,19 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
+# STATIC_URL = '/static/'
+# STATICFILES_DIRS = [
+#     BASE_DIR / "static",
+# ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = []
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
--- coolsite/food/views.py
+++ coolsite/food/views.py
@@ -1,31 +1,3 @@
-from django.http import HttpResponse,HttpResponseNotFound
-from django.shortcuts import redirect, render
-
-
-# def index(request):
-#     data = {
-#         'title': 'Главная старница!',
-#         'values': ['some' ,'hello']
-#     }
-#     return render(request, 'main/index.html', data)
-#
-# def about(request):
-#     return render(request, 'main/mainFood.html')
-
-# def pageNotFound(request,exception):
-#     return HttpResponse('<h1>Страница не найдена</h1>')
-
-
-# def acccesDenied(requset,exeption):
-#     return HttpResponseNotFound('<h1>Доступ запрещен</h1>')
-#
-# def serverError(requset,exeption):
-#     return HttpResponseNotFound('<h1>Невозможно обработать запрос</h1>')
-#
-# def forbidden(requset,exeption):
-#     return HttpResponseNotFound('<h1>Невозможно обработать запрос</h1>')
-
-
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse, HttpResponseNotFound, Http404
 from django.shortcuts import render, redirect, get_object_or_404
